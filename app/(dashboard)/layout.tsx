636ebdccec397eb33b4493525d4c17a3e2f8f749
Harsh Pandey <pandey73harsh@gmail.com>
2024-06-22 16:19:47 +0530

    add layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/app/Footer/Footer";
 
 const DashboardLayout =({ children }:{ children:React.ReactNode })=>{
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center justify-between bg-background ">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-background ">
       <Navbar/>
       {children}
       <Footer/>
