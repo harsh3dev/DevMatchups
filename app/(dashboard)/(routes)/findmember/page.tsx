c1d7a7c3c081427eed87ab188be9156bf6ee3552
Harsh Pandey <pandey73harsh@gmail.com>
2024-07-07 18:05:24 +0530

    fix build error

--- app/(dashboard)/(routes)/findmember/page.tsx
+++ app/(dashboard)/(routes)/findmember/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 import React from 'react'
 import Form from './Form/Form'
-import { useSession } from 'next-auth/react';
 
 const page = () => {
-
-  const session = useSession();
-  console.log('session', session);
   return (
     <div className=' min-h-screen text-black dark:bg-background bg-background dark:text-white relative '>
       <Form/>
