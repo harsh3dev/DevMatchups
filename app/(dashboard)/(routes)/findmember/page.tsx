56e5e96019d6ab9b86191315269f46f39c5531b3
Yashgupta9330 <yaahg342@gmail.com>
2024-07-10 16:50:03 +0530

    added

--- app/(dashboard)/(routes)/findmember/page.tsx
+++ app/(dashboard)/(routes)/findmember/page.tsx
@@ -5,8 +5,7 @@ import { useSession } from 'next-auth/react';
 
 const page = () => {
 
-  const session = useSession();
-  console.log('session', session);
+
   return (
     <div className=' min-h-screen text-black dark:bg-background bg-background dark:text-white relative '>
       <Form/>
