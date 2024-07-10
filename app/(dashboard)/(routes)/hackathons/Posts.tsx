0a26c125865ca98de68b3494dfe0db1297a261c3
Harsh Pandey <pandey73harsh@gmail.com>
2024-07-10 13:16:54 +0530

    add hackathons page

---
+++ app/(dashboard)/(routes)/hackathons/Posts.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import unstop from '@/app/assets/unstop.png'
+import devpost from '@/app/assets/devpost.svg'
+import { LuExternalLink } from "react-icons/lu";
+import Image from 'next/image'
+
+interface PostsProps{
+    title: string,
+    url: string,
+    logo: string,
+    platform: string,
+}
+
+const Posts:React.FC<PostsProps> = ({title, url, logo, platform}) => {
+    const URL = platform==='unstop' ? `https://unstop.com/${url}` : url
+  return (
+    <a href={URL} target='_blank' className=' group min-w-[20vw] min-h-[20vh] px-5 py-4 flex flex-col justify-between items-start rounded-lg border border-gray-500 dark:border-gray-800 dark:backdrop-blur-xl transition-all ease-linear hover:border-accent hover:dark:border-accent dark:bg-gray-900/60 hover:dark:bg-blue-900/30 bg-sky-100/70 hover:bg-sky-300/40  '>
+        <div className='flex justify-between p-2 items-center w-full ' >
+            <h1 className='text-xl font-bold '>
+                {title}
+            </h1>
+            <img src={logo} width={100} height={100} alt='logo-image' />
+        </div>
+        <div className='flex justify-between p-2 items-center w-full ' >
+            {
+                platform==='unstop' ? <Image src={unstop}  width={50} height={50} alt='logo-image' /> : <Image src={devpost} width={50} height={50} alt='logo-image' />
+            }
+
+            <span className=' group-hover:scale-200 '><LuExternalLink /></span>
+            
+        </div>
+    </a>
+  )
+}
+
+export default Posts
--- app/(dashboard)/(routes)/hackathons/page.tsx
+++ app/(dashboard)/(routes)/hackathons/page.tsx
@@ -1,11 +1,99 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Image from 'next/image'
+import unstop from '@/app/assets/unstop.png'
+import devpost from '@/app/assets/devpost.svg'
+import axios from 'axios'
+import {unstopData} from './mock_data'
+import { devpostData } from './mock_data'
+import Posts from './Posts'
 
 
 const page = () => {
+  const [unstopClicked, setUnstopClicked] = useState(true);
+  const [devpostClicked, setdevpostClicked] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [unstopPost, setUnstopPost] = useState([]);
+  const [devpostPost, setDevpostPost] = useState([]);
+
+  const unstopURL = 'https://unstop.com/api/public/opportunity/search-result?opportunity=hackathons&oppstatus=recent'
+  const devpostURL = 'https://devpost.com/api/hackathons?order_by=recently-added'
+
+  function handleClick() {
+    setUnstopClicked((prev)=>(!prev))
+    setdevpostClicked((prev)=>(!prev))
+  }
+
+  // useEffect(()=>{
+  //   const fetch = async () => {
+  //     // setLoading(true);
+  //     try {
+  //       const res1 = await axios.get(unstopURL);
+  //       const res2 = await axios.get(devpostURL);
+  //       setTimeout(() => {
+  //         console.log(res1.data);
+  //         console.log(res2.data);
+  //       }, 2000);
+  //     } catch (error) {
+  //         console.error('Error fetching data:', error);
+  //     } finally {
+  //         // setLoading(false);
+  //     }
+  // };
+  // fetch();
+  // },[])
+
+
   return (
-    <main className="flex min-h-screen  flex-col items-center justify-between bg-background ">
-      Coming Soon
-    </main>
+    <div className='min-h-screen w-full text-black dark:bg-background bg-background dark:text-white mb-20 '>
+      {/* <Tabs defaultValue="unstop" className=" mt-10 w-full  ">
+        <TabsList className='w-full'>
+          <TabsTrigger value="unstop">unstop</TabsTrigger>
+          <TabsTrigger value="devpost">devpost</TabsTrigger>
+        </TabsList>
+        <TabsContent value="unstop">Make changes to your unstop here.</TabsContent>
+        <TabsContent value="devpost">Change your devpost here.</TabsContent>
+      </Tabs> */}
+
+      <div className='w-full mt-16 flex flex-col justify-start items-start gap-5'>
+        <div className='w-full flex justify-start items-center gap-5 rounded-lg px-10' >
+          <div onClick={handleClick} className="p-2 flex items-center justify-center gap-4 rounded-md border w-[200px] h-[70px] border-text border-dashed dark:border-text dark:hover:bg-gray-200/80 cursor-pointer  " 
+          style={{backgroundColor: unstopClicked ? "var(--clickWhite)" : ""}}
+          >
+            <Image src={unstop} height={50} width={70} alt='unstop_icon' />
+          </div>
+          <div onClick={handleClick} className="p-2 flex items-center justify-center gap-4 rounded-md border w-[200px] h-[70px] border-text border-dashed dark:border-text dark:hover:bg-gray-200/80 cursor-pointer  " 
+          style={{backgroundColor: devpostClicked ? "var(--clickWhite)" : ""}}
+          >
+            <Image src={devpost} height={50} width={100} alt='devpost_icon' />
+          </div>
+        </div>
+        <hr className='w-full text-text dark:text-text h-4' />
+        {
+          unstopClicked &&
+          <div className='w-full min-h-[50vh] grid grid-cols-2 lg:grid-cols-3 gap-4 flex-wrap px-10 '>
+            {
+              unstopData.data.data.map((entry)=>(
+                <Posts key={entry.id} title={entry.title} url={entry.public_url} logo={entry.logoUrl2} platform='unstop' />
+              ))
+            }
+          </div>
+        }
+
+        {
+          devpostClicked && 
+          <div className='w-full min-h-[50vh] grid grid-cols-2 lg:grid-cols-3 gap-4 flex-wrap px-10 '>
+            {
+              devpostData.hackathons.map((entry)=>(
+                <Posts key={entry.id} title={entry.title} url={entry.url} logo={entry.thumbnail_url} platform='devpost' />
+              ))
+            }
+          </div>
+        }
+      </div>
+
+    </div>
   )
 }
 
