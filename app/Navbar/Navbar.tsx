0c402fd9aeadf086c5fa048788cac7c5d7543847
Sleepy Dev <118902211+GIRIRAJSHANKAR27@users.noreply.github.com>
2024-10-01 12:18:32 +0530

    fix: Add valid links to GitHub and Support buttons in mobile sidebar

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -207,12 +207,16 @@ const Navbar = () => {
                         <div className=' w-full flex flex-col gap-2 items-center justify-center'>
                             <div className=' w-full flex items-center justify-center gap-2 '>
                                 <Button variant="outline" className=' flex justify-center items-center gap-2 w-full ' >
+                                <Link href="https://github.com/harsh3dev/devmatchups" target="_blank" rel="noopener noreferrer">
                                     <IoLogoGithub />
                                     Github
+                                </Link>
                                 </Button>
                                 <Button variant="outline" className=' flex justify-center items-center gap-2 w-full ' >
+                                    <Link href="mailto:devmatchups@gmail.com">
                                     <MdEmail />
                                     Support
+                                    </Link>
                                 </Button>
                             </div>
 
