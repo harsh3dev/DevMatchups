6286ae63df216f7c40a9365c34601c5e40d9f631
Bishal-35 <karmakarbishal56@gmail.com>
2025-01-04 22:48:37 +0530

    Added Google Analytics #66

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Analytics } from "@vercel/analytics/react"
+// import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import StoreProvider from "./StoreProvider";
 import { NextAuthProvider } from "./provider";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata: Metadata = {
   title: {
@@ -42,28 +42,38 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
       </head>
-      <body className="bg-background" cz-shortcut-listen="false" >
+      <body className="bg-background" cz-shortcut-listen="false">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
-          
         >
           <NextAuthProvider>
             <StoreProvider>
               {children}
-              <Analytics />
+              {/* <Analytics /> */}
             </StoreProvider>
           </NextAuthProvider>
           <ToastContainer />
-
         </ThemeProvider>
       </body>
     </html>
