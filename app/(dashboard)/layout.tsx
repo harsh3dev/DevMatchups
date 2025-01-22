e32b808909d0af9869229350bf46a0a09a5d0a48
Soumya Ganguly <sounyaganguly89@gmail.com>
2025-01-22 18:05:09 +0530

    done

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from "@/app/Navbar/Navbar";
-import Footer from "@/app/Footer/Footer";
+import React from 'react';
+import Navbar from '@/app/Navbar/Navbar';
+import Footer from '@/app/Footer/Footer';
 
-const DashboardLayout =({ children }:{ children:React.ReactNode })=>{
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-background ">
-      <Navbar/>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-background overflow-x-hidden md:overflow-x-auto"> 
+      <Navbar />
       {children}
-      <Footer/>
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
