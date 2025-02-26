19e0dc8e0350bdd0905a25f613cd3a2c12d8e82f
Aditya <adityasahoo652@gmail.com>
2025-02-26 09:54:25 +0530

    feat: update devpost API URL to fetch 20 hackathons per request

--- app/api/devpostHackathon/route.ts
+++ app/api/devpostHackathon/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET(req: Request) {
-  const devpostURL = 'https://devpost.com/api/hackathons?order_by=recently-added'
+  const devpostURL = 'https://devpost.com/api/hackathons?order_by=recently-added&per_page=20'
   try {
     const response = await axios.get(devpostURL);
 
