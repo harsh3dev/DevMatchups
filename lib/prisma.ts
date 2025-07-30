154169e60a8ff0ffb7b0efb2a3df2d4f53cac868
Harsh Pandey <pandey73harsh@gmail.com>
2025-07-30 22:41:02 +0530

    merge

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+class PrismaClientSingleton {
+  private static instance: PrismaClient;
 
-export { prisma };
+  public static getInstance(): PrismaClient {
+    if (!PrismaClientSingleton.instance) {
+      PrismaClientSingleton.instance = new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      });
+    }
+    return PrismaClientSingleton.instance;
+  }
+}
+
+export const prisma = PrismaClientSingleton.getInstance();
